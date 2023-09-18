8773c3b2abae3190cf710e0c3f8c004a13fb8040
Nikita <nikita91797@gmail.com>
2023-09-18 23:50:44 +0800

    add example_dz2

---
+++ Examples/example_dz2/Program.cs
@@ -0,0 +1,9 @@
+﻿int numA=Convert.ToInt32(Console.ReadLine());
+int numB=Convert.ToInt32(Console.ReadLine());
+int numC=Convert.ToInt32(Console.ReadLine());
+Console.Write("Наибольшее число ");
+if(numA>numB){
+    Console.WriteLine(numA>numC ? numA:numC);
+}else{
+     Console.WriteLine(numB>numC ? numB:numC);
+}
