86d9076dca694c705d40453aedc1277147bec014
Nikita <nikita91797@gmail.com>
2023-09-18 16:45:39 +0800

    add example 005 ifElse

--- Examples/example005_ifElse/Program.cs
+++ Examples/example005_ifElse/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string Name = Console.ReadLine();
+if (Name=="Nikita"){
+    Console.Write("Hello ");
+    Console.WriteLine(Name);    
+}else{
+    Console.Write("______");
+    Console.Write(Name);
+    Console.WriteLine("_____");
+}
