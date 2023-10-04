4851305a35abeff5490cced6377b181d39aecd81
Nikita <nikita91797@gmail.com>
2023-10-04 17:18:01 +0800

    add example_HomeWork5_dz2

---
+++ Examples/example_HomeWork5_dz2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+string sum(string numA){
+    int count=0;
+    for( int i=0;i<numA.Length;i++){
+        count+=numA[i]-'0';
+    }
+    return numA+" -> "+Convert.ToString(count);
+}
+Console.WriteLine(sum(Console.ReadLine()));
+
+
+
