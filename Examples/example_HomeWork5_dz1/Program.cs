9689a7231739e87a5086b7d88cb7cf8a82d594fa
Nikita <nikita91797@gmail.com>
2023-10-04 16:48:34 +0800

    add example_HomeWork5_dz1

---
+++ Examples/example_HomeWork5_dz1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+int power(int numA,int numB){
+    int numC=1;
+    for( int i=0;i<numB;i++){
+        numC*=numA; 
+    }
+    return numC;
+}
+
+string[] a=Console.ReadLine().Split(new char[] {' ',','}, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine(power(Convert.ToInt32(a[0]),Convert.ToInt32(a[1])));
+
+
