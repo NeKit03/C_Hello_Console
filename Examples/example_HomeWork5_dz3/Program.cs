8b5bb1baf90cfd5d069430e28cfb74b742ce699a
Nikita <nikita91797@gmail.com>
2023-10-04 17:18:28 +0800

    add example_HomeWork5_dz3

---
+++ Examples/example_HomeWork5_dz3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+string[] array(string s){
+    return s.Split(new char[] {' ',','}, StringSplitOptions.RemoveEmptyEntries);
+
+}
+
+string[] a= array(Console.ReadLine());
+for(int i=0;i<a.Length;i++){
+    Console.Write($"{a[i]} ");
+}
