9396bbe8d7429380735496addb5ee2d4ce022691
Nikita <nikita91797@gmail.com>
2023-10-09 17:13:30 +0800

    add example_HomeWork7_dz1

---
+++ Examples/example_HomeWork7_dz1/Program.cs
@@ -0,0 +1,30 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+int[] IntArray(){
+    string[] s=Array();
+    int [] array = new int[s.Length];
+    for(int i=0;i<s.Length;i++){
+        array[i]=Convert.ToInt32(s[i]);
+    }
+    return array;
+}
+string[] Array(){
+    return Console.ReadLine().Split(new char[] {' ',','}, StringSplitOptions.RemoveEmptyEntries); 
+}
+int Quantity(int[] array){
+    int count=0;
+    foreach(int i in array)
+        if(i>0)
+            count++;
+    return count;        
+}
+
+void IntPrintArray(int[] array){
+    for(int i=0;i<array.Length-1;i++)
+        Console.Write($"{array[i]},");
+    Console.Write($"{array[array.Length-1]}->");    
+}
+
+int[] array=IntArray();
+IntPrintArray(array);
+Console.Write(Quantity(array));
+
