0cebd19230bcecf60f1ecd1a7c0be9aa61b1cba9
Nikita <nikita91797@gmail.com>
2023-10-22 21:44:48 +0800

    add example_HomeWork10_dz1

---
+++ Examples/example_HomeWork10_dz1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+public class Answer
+{
+  public static void PrintLineNumber(int a)
+    {
+    if(a>1){
+        Console.Write($"{a}, "); 
+        PrintLineNumber(--a);
+    }
+    else{
+        Console.Write($"{a}");
+    }
+    
+
+    }
+
+ // Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+    string[] s=Console.ReadLine().Split(new char[] {' ',',','N','='}, StringSplitOptions.RemoveEmptyEntries);
+    PrintLineNumber(Convert.ToInt32(s[0]));
+    }
+} 
